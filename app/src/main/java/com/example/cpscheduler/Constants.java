4d08e8eb442da138dc84348064d5610d8dc93228
Mohakg17 <manmps17@gmail.com>
2024-05-15 00:40:13 +0530

    added environment variables

---
+++ app/src/main/java/com/example/cpscheduler/Constants.java
@@ -0,0 +1,10 @@
+package com.example.cpscheduler;
+
+public class Constants {
+    String CODECHEF_API = "https://competeapi.vercel.app/contests/codechef/";
+    String CODEFORCES_API = "https://competeapi.vercel.app/contests/codeforces/";
+    String LEETCODE_API = "https://competeapi.vercel.app/contests/leetcode/";
+    String CODEFORCES_PROFILE_API = "https://codeforces.com/api/user.info?handles=";
+    String LEETCODE_PROFILE_API = "https://leetcode-stats-api.herokuapp.com/";
+
+}
